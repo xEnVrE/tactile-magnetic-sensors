8c93330ab422fcc5b1140b19d7a2539dd6ffa5c3
Nicola Piga <nicolapiga@gmail.com>
2019-02-07 12:22:50 +0100

    Implement class ReadingModule

---
+++ src/module/include/ReadingModule.h
@@ -0,0 +1,54 @@
+#ifndef READINGMODULE_H
+#define READINGMODULE_H
+
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/ResourceFinder.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/sig/Vector.h>
+
+#include <skinSensor.h>
+
+#include <thrift/TactileMagneticSensoreModuleIDL.h>
+
+
+class ReadingModule : public yarp::os::RFModule,
+                      public TactileMagneticSensoreModuleIDL
+{
+public:
+    virtual ~ReadingModule();
+
+    /**
+     * RFModule interface.
+     */
+    bool configure(yarp::os::ResourceFinder& rf) override;
+
+    bool updateModule() override;
+
+    double getPeriod() override;
+
+    bool close() override;
+
+    /**
+     * IDL interface.
+     */
+
+    bool calibrate() override;
+
+    bool quit() override;
+
+protected:
+    std::pair<bool, std::vector<unsigned int>> loadListUnsigned(yarp::os::ResourceFinder& rf, const std::string& key);
+
+    std::unique_ptr<skinSensor> skin_sensor_drv_;
+
+    yarp::os::Port port_rpc_command_;
+
+    yarp::os::BufferedPort<yarp::sig::VectorOf<int>> port_data_out_;
+
+    const std::string log_ID_ = "[TACTILE_MAGNETIC_SENSORS_MODULE]";
+
+    double period_;
+};
+
+#endif /* READINGMODULE_H */
---
+++ src/module/src/ReadingModule.cpp
@@ -0,0 +1,162 @@
+#include <ReadingModule.h>
+
+#include <yarp/os/LogStream.h>
+#include <yarp/os/ResourceFinder.h>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+
+
+ReadingModule::~ReadingModule()
+{ }
+
+
+bool ReadingModule::configure(yarp::os::ResourceFinder& rf)
+{
+    // Get the port prefix
+    const std::string port_prefix = rf.check("port_prefix", Value("tactile-magnetic-sensor")).asString();
+
+    // Get the period
+    period_ = rf.check("period", Value(1.0)).asDouble();
+
+    // Get the name of the can device
+    const std::string can_name = rf.check("can_name", Value("can0")).asString();
+
+    // Get the size of the window used to average on each sensor measurements
+    const std::size_t average_window_size = rf.check("average_window_size", Value(1)).asInt();
+
+    // Get the list of sensor ids
+    bool valid_ids;
+    std::vector<unsigned int> list_ids;
+    std::tie(valid_ids, list_ids) = loadListUnsigned(rf, "sensor_ids");
+    if ((!valid_ids) || list_ids.size() == 0)
+    {
+        yError() << log_ID_ << "Cannot retrieve the sensors ids.";
+
+        return false;
+    }
+
+    // Initialize the driver
+    skin_sensor_drv_ = std::unique_ptr<skinSensor>(new skinSensor(can_name, list_ids, average_window_size));
+
+    // Open data output port
+    if (!port_data_out_.open("/" + port_prefix + "/data:o"))
+    {
+        yError() << log_ID_ << "Cannot open the data output port.";
+
+        return false;
+    }
+
+    // Open RPC input port for commands
+    if (!port_rpc_command_.open("/" + port_prefix + "/cmd:i"))
+    {
+        yError() << log_ID_ << "Cannot open the RPC command input port.";
+
+        return false;
+    }
+
+    if (!(this->yarp().attachAsServer(port_rpc_command_)))
+    {
+        yError() << log_ID_ << "Cannot attach the RPC command input port.";
+
+        return false;
+    }
+
+    // Log
+    yInfo() << log_ID_ << "Loaded configuration:";
+    yInfo() << log_ID_ << "- port_prefix:" << port_prefix;
+    yInfo() << log_ID_ << "- period:" << period_;
+    yInfo() << log_ID_ << "- sensors_ids:";
+    for (unsigned int& id : list_ids)
+        yInfo() << log_ID_ << id;
+
+    yInfo() << log_ID_ << "RPC command port opened and attached. Ready to recieve commands!";
+}
+
+
+bool ReadingModule::updateModule()
+{
+    try
+    {
+        // Update sensors
+        skin_sensor_drv_->updateSensors();
+
+        std::vector<int> sensors_data;
+        sensors_data = skin_sensor_drv_->getData();
+
+        // Fill the output yarp vector
+        VectorOf<int>& data_out = port_data_out_.prepare();
+
+        data_out = VectorOf<int>(sensors_data.size(), sensors_data.data());
+
+        // Send the data
+        port_data_out_.write();
+
+    }
+    catch (const std::exception& e)
+    {
+        yError() << "Exception" << e.what();
+        return false;
+    }
+
+    return true;
+}
+
+
+double ReadingModule::getPeriod()
+{
+    return period_;
+}
+
+
+bool ReadingModule::close()
+{
+    return true;
+}
+
+
+bool ReadingModule::calibrate()
+{
+    //TODO
+    return true;
+}
+
+
+bool ReadingModule::quit()
+{
+    stopModule();
+
+    return true;
+}
+
+
+std::pair<bool, std::vector<unsigned int>> ReadingModule::loadListUnsigned(ResourceFinder &rf, const std::string& key)
+{
+    bool ok = true;
+
+    if (rf.find(key).isNull())
+        ok = false;
+
+    Bottle* b = rf.find(key).asList();
+    if (b == nullptr)
+        ok = false;
+
+    if (!ok)
+        return std::make_pair(false, std::vector<unsigned int>());
+
+    std::vector<unsigned int> list;
+    for (std::size_t i = 0; i < b->size(); i++)
+    {
+        Value item_v = b->get(i);
+        if (item_v.isNull())
+            return std::make_pair(false, std::vector<unsigned int>());
+
+        if (!item_v.isString())
+            return std::make_pair(false, std::vector<unsigned int>());
+
+        // Is it ok to use 'asInt' for unsigned int?
+        list.push_back(item_v.asInt());
+    }
+
+    return std::make_pair(true, list);
+}
