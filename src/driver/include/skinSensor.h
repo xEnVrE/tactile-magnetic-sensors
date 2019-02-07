e35a4f9215e11fa40ffcf699b6a19a4bb2d20646
Nicola Piga <nicolapiga@gmail.com>
2019-02-07 10:23:45 +0100

    Fix indentation in src/driver/include/skinSensor.h

--- src/driver/include/skinSensor.h
+++ src/driver/include/skinSensor.h
@@ -32,12 +32,12 @@ protected:
 
     const unsigned MTB_ID = 0x201;
 
-	//Ids of the incoming sensor messages
-	// const unsigned SensorIds[8] = [0x750, 0x741, 0x761, 0x744, 0x754, 0x764, 0x774, 0x775]
-	// const unsigned numberOfSensors = 8;
-	//TESTING 1 SENSOR
-	const unsigned SensorIds[1] = {1876}; //1872 = 0x750, 1876 = 0x754
-	const unsigned numberOfSensors = 1;
+    //Ids of the incoming sensor messages
+    // const unsigned SensorIds[8] = [0x750, 0x741, 0x761, 0x744, 0x754, 0x764, 0x774, 0x775]
+    // const unsigned numberOfSensors = 8;
+    //TESTING 1 SENSOR
+    const unsigned SensorIds[1] = {1876}; //1872 = 0x750, 1876 = 0x754
+    const unsigned numberOfSensors = 1;
 
     // Number of readings to obtain an average measure from the sensor
     const unsigned numReadings = 2;
